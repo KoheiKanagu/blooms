fbbd439ac85726ebe4cfe517d8cdd54b3d811913
KoheiKanagu <kanagu@kingu.dev>
2025-01-14 16:33:41 +0900

    logger

---
+++ app/lib/utils/my_logger.dart
@@ -0,0 +1,62 @@
+import 'package:blooms/constants/app_env.dart';
+import 'package:firebase_crashlytics/firebase_crashlytics.dart';
+import 'package:flutter/foundation.dart';
+import 'package:riverpod_annotation/riverpod_annotation.dart';
+import 'package:talker_flutter/talker_flutter.dart';
+import 'package:talker_riverpod_logger/talker_riverpod_logger.dart';
+
+final logger = TalkerFlutter.init(
+  observer: kReleaseMode ? CrashlyticsTalkerObserver() : null,
+);
+
+class CrashlyticsTalkerObserver extends TalkerObserver {
+  CrashlyticsTalkerObserver();
+
+  @override
+  void onError(TalkerError err) {
+    FirebaseCrashlytics.instance.recordError(
+      err.error,
+      err.stackTrace,
+      reason: err.message,
+    );
+  }
+
+  @override
+  void onException(TalkerException err) {
+    FirebaseCrashlytics.instance.recordError(
+      err.exception,
+      err.stackTrace,
+      reason: err.message,
+    );
+  }
+
+  @override
+  void onLog(TalkerData log) {
+    FirebaseCrashlytics.instance.log(
+      log.generateTextMessage(),
+    );
+  }
+}
+
+/// [Riverpod] logger on [Talker] base
+///
+/// 機密情報が含まれるProviderはログを抑制
+final talkerRiverpodObserver = TalkerRiverpodObserver(
+  talker: logger,
+  settings: TalkerRiverpodLoggerSettings(
+    providerFilter: (provider) {
+      if (!kAppEnvProd) {
+        final hidden = {
+          'firebaseUserProvider',
+          'firebaseUserIdTokenResultProvider',
+        };
+
+        //　機密情報が含まれるため、[kAppEnvProd]の場合はログを抑制
+        if (hidden.contains(provider.name)) {
+          return false;
+        }
+      }
+      return true;
+    },
+  ),
+);
