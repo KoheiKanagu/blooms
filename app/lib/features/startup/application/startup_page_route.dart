ac070a0002b81b5b283f7c093e92ec09b0fbbb22
KoheiKanagu <kanagu@kingu.dev>
2025-01-15 10:44:25 +0900

    route

---
+++ app/lib/features/startup/application/startup_page_route.dart
@@ -0,0 +1,22 @@
+import 'package:blooms/features/startup/presentation/startup_page.dart';
+import 'package:flutter/widgets.dart';
+import 'package:go_router/go_router.dart';
+
+part 'startup_page_route.g.dart';
+
+@TypedGoRoute<AppStartupPageRoute>(
+  path: AppStartupPageRoute.path,
+)
+class AppStartupPageRoute extends GoRouteData {
+  const AppStartupPageRoute();
+
+  static const path = '/startup';
+
+  @override
+  Page<void> buildPage(BuildContext context, GoRouterState state) {
+    return const NoTransitionPage(
+      child: AppStartupWidget(),
+      name: path,
+    );
+  }
+}
