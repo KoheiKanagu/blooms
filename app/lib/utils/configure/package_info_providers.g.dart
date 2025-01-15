6524feabb48526d9b6da9f8536187f176f06295b
KoheiKanagu <kanagu@kingu.dev>
2025-01-15 10:43:08 +0900

    package info

---
+++ app/lib/utils/configure/package_info_providers.g.dart
@@ -0,0 +1,118 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+// ignore_for_file: require_trailing_commas, non_constant_identifier_names, cast_nullable_to_non_nullable, lines_longer_than_80_chars, unnecessary_null_checks, strict_raw_type
+
+part of 'package_info_providers.dart';
+
+// **************************************************************************
+// RiverpodGenerator
+// **************************************************************************
+
+String _$packageInfoHash() => r'854bbb0e381edfdddbd736229351d6cc918a2ad1';
+
+/// See also [packageInfo].
+@ProviderFor(packageInfo)
+final packageInfoProvider = FutureProvider<PackageInfo>.internal(
+  packageInfo,
+  name: r'packageInfoProvider',
+  debugGetCreateSourceHash:
+      const bool.fromEnvironment('dart.vm.product') ? null : _$packageInfoHash,
+  dependencies: null,
+  allTransitiveDependencies: null,
+);
+
+@Deprecated('Will be removed in 3.0. Use Ref instead')
+// ignore: unused_element
+typedef PackageInfoRef = FutureProviderRef<PackageInfo>;
+String _$packageInfoBuildNumberHash() =>
+    r'7639a8503222a4978556762e25cb40acff4b8fdf';
+
+/// See also [packageInfoBuildNumber].
+@ProviderFor(packageInfoBuildNumber)
+final packageInfoBuildNumberProvider = AutoDisposeProvider<int>.internal(
+  packageInfoBuildNumber,
+  name: r'packageInfoBuildNumberProvider',
+  debugGetCreateSourceHash: const bool.fromEnvironment('dart.vm.product')
+      ? null
+      : _$packageInfoBuildNumberHash,
+  dependencies: null,
+  allTransitiveDependencies: null,
+);
+
+@Deprecated('Will be removed in 3.0. Use Ref instead')
+// ignore: unused_element
+typedef PackageInfoBuildNumberRef = AutoDisposeProviderRef<int>;
+String _$packageInfoVersionHash() =>
+    r'b122137425323c9433be0550cc9c78419dd73de6';
+
+/// See also [packageInfoVersion].
+@ProviderFor(packageInfoVersion)
+final packageInfoVersionProvider = AutoDisposeProvider<String>.internal(
+  packageInfoVersion,
+  name: r'packageInfoVersionProvider',
+  debugGetCreateSourceHash: const bool.fromEnvironment('dart.vm.product')
+      ? null
+      : _$packageInfoVersionHash,
+  dependencies: null,
+  allTransitiveDependencies: null,
+);
+
+@Deprecated('Will be removed in 3.0. Use Ref instead')
+// ignore: unused_element
+typedef PackageInfoVersionRef = AutoDisposeProviderRef<String>;
+String _$packageInfoAppNameHash() =>
+    r'06ce5b04ace30069d593233a04c066c901f39319';
+
+/// See also [packageInfoAppName].
+@ProviderFor(packageInfoAppName)
+final packageInfoAppNameProvider = AutoDisposeProvider<String>.internal(
+  packageInfoAppName,
+  name: r'packageInfoAppNameProvider',
+  debugGetCreateSourceHash: const bool.fromEnvironment('dart.vm.product')
+      ? null
+      : _$packageInfoAppNameHash,
+  dependencies: null,
+  allTransitiveDependencies: null,
+);
+
+@Deprecated('Will be removed in 3.0. Use Ref instead')
+// ignore: unused_element
+typedef PackageInfoAppNameRef = AutoDisposeProviderRef<String>;
+String _$packageInfoPackageNameHash() =>
+    r'1bc859516fbdc5cfa901f11852b3600fa762a2d6';
+
+/// See also [packageInfoPackageName].
+@ProviderFor(packageInfoPackageName)
+final packageInfoPackageNameProvider = AutoDisposeProvider<String>.internal(
+  packageInfoPackageName,
+  name: r'packageInfoPackageNameProvider',
+  debugGetCreateSourceHash: const bool.fromEnvironment('dart.vm.product')
+      ? null
+      : _$packageInfoPackageNameHash,
+  dependencies: null,
+  allTransitiveDependencies: null,
+);
+
+@Deprecated('Will be removed in 3.0. Use Ref instead')
+// ignore: unused_element
+typedef PackageInfoPackageNameRef = AutoDisposeProviderRef<String>;
+String _$packageInfoInstallerStoreHash() =>
+    r'cac060677ffb9b1ed78cce814b6904f25742ecb5';
+
+/// See also [packageInfoInstallerStore].
+@ProviderFor(packageInfoInstallerStore)
+final packageInfoInstallerStoreProvider = AutoDisposeProvider<String?>.internal(
+  packageInfoInstallerStore,
+  name: r'packageInfoInstallerStoreProvider',
+  debugGetCreateSourceHash: const bool.fromEnvironment('dart.vm.product')
+      ? null
+      : _$packageInfoInstallerStoreHash,
+  dependencies: null,
+  allTransitiveDependencies: null,
+);
+
+@Deprecated('Will be removed in 3.0. Use Ref instead')
+// ignore: unused_element
+typedef PackageInfoInstallerStoreRef = AutoDisposeProviderRef<String?>;
+// ignore_for_file: type=lint
+// ignore_for_file: subtype_of_sealed_class, invalid_use_of_internal_member, invalid_use_of_visible_for_testing_member, deprecated_member_use_from_same_package
