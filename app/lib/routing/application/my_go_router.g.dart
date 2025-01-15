72bb5c4748b421f00056c02e38e107d17b3c2159
KoheiKanagu <kanagu@kingu.dev>
2025-01-15 17:31:16 +0900

    go_router

---
+++ app/lib/routing/application/my_go_router.g.dart
@@ -0,0 +1,28 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+// ignore_for_file: require_trailing_commas, non_constant_identifier_names, cast_nullable_to_non_nullable, lines_longer_than_80_chars, unnecessary_null_checks, strict_raw_type
+
+part of 'my_go_router.dart';
+
+// **************************************************************************
+// RiverpodGenerator
+// **************************************************************************
+
+String _$myGoRouterHash() => r'71b56ad55d7d81f3f1a01635fde0b5bed8f064e3';
+
+/// See also [myGoRouter].
+@ProviderFor(myGoRouter)
+final myGoRouterProvider = AutoDisposeProvider<GoRouter>.internal(
+  myGoRouter,
+  name: r'myGoRouterProvider',
+  debugGetCreateSourceHash:
+      const bool.fromEnvironment('dart.vm.product') ? null : _$myGoRouterHash,
+  dependencies: null,
+  allTransitiveDependencies: null,
+);
+
+@Deprecated('Will be removed in 3.0. Use Ref instead')
+// ignore: unused_element
+typedef MyGoRouterRef = AutoDisposeProviderRef<GoRouter>;
+// ignore_for_file: type=lint
+// ignore_for_file: subtype_of_sealed_class, invalid_use_of_internal_member, invalid_use_of_visible_for_testing_member, deprecated_member_use_from_same_package
