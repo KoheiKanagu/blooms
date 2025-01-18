4a58711c6ae399045a82791a550eceb575077a18
KoheiKanagu <kanagu@kingu.dev>
2025-01-18 20:42:34 +0900

    feat: 体調の記録ができるように

---
+++ app/lib/features/condition/application/condition_providers.g.dart
@@ -0,0 +1,334 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+// ignore_for_file: require_trailing_commas, non_constant_identifier_names, cast_nullable_to_non_nullable, lines_longer_than_80_chars, unnecessary_null_checks, strict_raw_type
+
+part of 'condition_providers.dart';
+
+// **************************************************************************
+// RiverpodGenerator
+// **************************************************************************
+
+String _$conditionCollectionReferenceHash() =>
+    r'f8307e781b15030663350a9f08858e8da6634ad0';
+
+/// See also [conditionCollectionReference].
+@ProviderFor(conditionCollectionReference)
+final conditionCollectionReferenceProvider =
+    AutoDisposeProvider<CollectionReference<Condition>>.internal(
+  conditionCollectionReference,
+  name: r'conditionCollectionReferenceProvider',
+  debugGetCreateSourceHash: const bool.fromEnvironment('dart.vm.product')
+      ? null
+      : _$conditionCollectionReferenceHash,
+  dependencies: null,
+  allTransitiveDependencies: null,
+);
+
+@Deprecated('Will be removed in 3.0. Use Ref instead')
+// ignore: unused_element
+typedef ConditionCollectionReferenceRef
+    = AutoDisposeProviderRef<CollectionReference<Condition>>;
+String _$conditionQueryHash() => r'f5112535b42537ba0dfe4329bb4a2edf642bafbe';
+
+/// See also [conditionQuery].
+@ProviderFor(conditionQuery)
+final conditionQueryProvider =
+    AutoDisposeFutureProvider<Query<Condition>>.internal(
+  conditionQuery,
+  name: r'conditionQueryProvider',
+  debugGetCreateSourceHash: const bool.fromEnvironment('dart.vm.product')
+      ? null
+      : _$conditionQueryHash,
+  dependencies: null,
+  allTransitiveDependencies: null,
+);
+
+@Deprecated('Will be removed in 3.0. Use Ref instead')
+// ignore: unused_element
+typedef ConditionQueryRef = AutoDisposeFutureProviderRef<Query<Condition>>;
+String _$conditionDeleteHash() => r'cefe0c4927890c81f53ddc2b4df9776fdbfb6934';
+
+/// Copied from Dart SDK
+class _SystemHash {
+  _SystemHash._();
+
+  static int combine(int hash, int value) {
+    // ignore: parameter_assignments
+    hash = 0x1fffffff & (hash + value);
+    // ignore: parameter_assignments
+    hash = 0x1fffffff & (hash + ((0x0007ffff & hash) << 10));
+    return hash ^ (hash >> 6);
+  }
+
+  static int finish(int hash) {
+    // ignore: parameter_assignments
+    hash = 0x1fffffff & (hash + ((0x03ffffff & hash) << 3));
+    // ignore: parameter_assignments
+    hash = hash ^ (hash >> 11);
+    return 0x1fffffff & (hash + ((0x00003fff & hash) << 15));
+  }
+}
+
+/// See also [conditionDelete].
+@ProviderFor(conditionDelete)
+const conditionDeleteProvider = ConditionDeleteFamily();
+
+/// See also [conditionDelete].
+class ConditionDeleteFamily extends Family<AsyncValue<void>> {
+  /// See also [conditionDelete].
+  const ConditionDeleteFamily();
+
+  /// See also [conditionDelete].
+  ConditionDeleteProvider call({
+    required QueryDocumentSnapshot<Condition> snapshot,
+  }) {
+    return ConditionDeleteProvider(
+      snapshot: snapshot,
+    );
+  }
+
+  @override
+  ConditionDeleteProvider getProviderOverride(
+    covariant ConditionDeleteProvider provider,
+  ) {
+    return call(
+      snapshot: provider.snapshot,
+    );
+  }
+
+  static const Iterable<ProviderOrFamily>? _dependencies = null;
+
+  @override
+  Iterable<ProviderOrFamily>? get dependencies => _dependencies;
+
+  static const Iterable<ProviderOrFamily>? _allTransitiveDependencies = null;
+
+  @override
+  Iterable<ProviderOrFamily>? get allTransitiveDependencies =>
+      _allTransitiveDependencies;
+
+  @override
+  String? get name => r'conditionDeleteProvider';
+}
+
+/// See also [conditionDelete].
+class ConditionDeleteProvider extends AutoDisposeFutureProvider<void> {
+  /// See also [conditionDelete].
+  ConditionDeleteProvider({
+    required QueryDocumentSnapshot<Condition> snapshot,
+  }) : this._internal(
+          (ref) => conditionDelete(
+            ref as ConditionDeleteRef,
+            snapshot: snapshot,
+          ),
+          from: conditionDeleteProvider,
+          name: r'conditionDeleteProvider',
+          debugGetCreateSourceHash:
+              const bool.fromEnvironment('dart.vm.product')
+                  ? null
+                  : _$conditionDeleteHash,
+          dependencies: ConditionDeleteFamily._dependencies,
+          allTransitiveDependencies:
+              ConditionDeleteFamily._allTransitiveDependencies,
+          snapshot: snapshot,
+        );
+
+  ConditionDeleteProvider._internal(
+    super._createNotifier, {
+    required super.name,
+    required super.dependencies,
+    required super.allTransitiveDependencies,
+    required super.debugGetCreateSourceHash,
+    required super.from,
+    required this.snapshot,
+  }) : super.internal();
+
+  final QueryDocumentSnapshot<Condition> snapshot;
+
+  @override
+  Override overrideWith(
+    FutureOr<void> Function(ConditionDeleteRef provider) create,
+  ) {
+    return ProviderOverride(
+      origin: this,
+      override: ConditionDeleteProvider._internal(
+        (ref) => create(ref as ConditionDeleteRef),
+        from: from,
+        name: null,
+        dependencies: null,
+        allTransitiveDependencies: null,
+        debugGetCreateSourceHash: null,
+        snapshot: snapshot,
+      ),
+    );
+  }
+
+  @override
+  AutoDisposeFutureProviderElement<void> createElement() {
+    return _ConditionDeleteProviderElement(this);
+  }
+
+  @override
+  bool operator ==(Object other) {
+    return other is ConditionDeleteProvider && other.snapshot == snapshot;
+  }
+
+  @override
+  int get hashCode {
+    var hash = _SystemHash.combine(0, runtimeType.hashCode);
+    hash = _SystemHash.combine(hash, snapshot.hashCode);
+
+    return _SystemHash.finish(hash);
+  }
+}
+
+@Deprecated('Will be removed in 3.0. Use Ref instead')
+// ignore: unused_element
+mixin ConditionDeleteRef on AutoDisposeFutureProviderRef<void> {
+  /// The parameter `snapshot` of this provider.
+  QueryDocumentSnapshot<Condition> get snapshot;
+}
+
+class _ConditionDeleteProviderElement
+    extends AutoDisposeFutureProviderElement<void> with ConditionDeleteRef {
+  _ConditionDeleteProviderElement(super.provider);
+
+  @override
+  QueryDocumentSnapshot<Condition> get snapshot =>
+      (origin as ConditionDeleteProvider).snapshot;
+}
+
+String _$conditionCreateSubjectiveHash() =>
+    r'e1cd42e97ee68b58406e3b6710a8ee46cc3b33ed';
+
+/// See also [conditionCreateSubjective].
+@ProviderFor(conditionCreateSubjective)
+const conditionCreateSubjectiveProvider = ConditionCreateSubjectiveFamily();
+
+/// See also [conditionCreateSubjective].
+class ConditionCreateSubjectiveFamily extends Family<AsyncValue<void>> {
+  /// See also [conditionCreateSubjective].
+  const ConditionCreateSubjectiveFamily();
+
+  /// See also [conditionCreateSubjective].
+  ConditionCreateSubjectiveProvider call({
+    required String record,
+  }) {
+    return ConditionCreateSubjectiveProvider(
+      record: record,
+    );
+  }
+
+  @override
+  ConditionCreateSubjectiveProvider getProviderOverride(
+    covariant ConditionCreateSubjectiveProvider provider,
+  ) {
+    return call(
+      record: provider.record,
+    );
+  }
+
+  static const Iterable<ProviderOrFamily>? _dependencies = null;
+
+  @override
+  Iterable<ProviderOrFamily>? get dependencies => _dependencies;
+
+  static const Iterable<ProviderOrFamily>? _allTransitiveDependencies = null;
+
+  @override
+  Iterable<ProviderOrFamily>? get allTransitiveDependencies =>
+      _allTransitiveDependencies;
+
+  @override
+  String? get name => r'conditionCreateSubjectiveProvider';
+}
+
+/// See also [conditionCreateSubjective].
+class ConditionCreateSubjectiveProvider
+    extends AutoDisposeFutureProvider<void> {
+  /// See also [conditionCreateSubjective].
+  ConditionCreateSubjectiveProvider({
+    required String record,
+  }) : this._internal(
+          (ref) => conditionCreateSubjective(
+            ref as ConditionCreateSubjectiveRef,
+            record: record,
+          ),
+          from: conditionCreateSubjectiveProvider,
+          name: r'conditionCreateSubjectiveProvider',
+          debugGetCreateSourceHash:
+              const bool.fromEnvironment('dart.vm.product')
+                  ? null
+                  : _$conditionCreateSubjectiveHash,
+          dependencies: ConditionCreateSubjectiveFamily._dependencies,
+          allTransitiveDependencies:
+              ConditionCreateSubjectiveFamily._allTransitiveDependencies,
+          record: record,
+        );
+
+  ConditionCreateSubjectiveProvider._internal(
+    super._createNotifier, {
+    required super.name,
+    required super.dependencies,
+    required super.allTransitiveDependencies,
+    required super.debugGetCreateSourceHash,
+    required super.from,
+    required this.record,
+  }) : super.internal();
+
+  final String record;
+
+  @override
+  Override overrideWith(
+    FutureOr<void> Function(ConditionCreateSubjectiveRef provider) create,
+  ) {
+    return ProviderOverride(
+      origin: this,
+      override: ConditionCreateSubjectiveProvider._internal(
+        (ref) => create(ref as ConditionCreateSubjectiveRef),
+        from: from,
+        name: null,
+        dependencies: null,
+        allTransitiveDependencies: null,
+        debugGetCreateSourceHash: null,
+        record: record,
+      ),
+    );
+  }
+
+  @override
+  AutoDisposeFutureProviderElement<void> createElement() {
+    return _ConditionCreateSubjectiveProviderElement(this);
+  }
+
+  @override
+  bool operator ==(Object other) {
+    return other is ConditionCreateSubjectiveProvider && other.record == record;
+  }
+
+  @override
+  int get hashCode {
+    var hash = _SystemHash.combine(0, runtimeType.hashCode);
+    hash = _SystemHash.combine(hash, record.hashCode);
+
+    return _SystemHash.finish(hash);
+  }
+}
+
+@Deprecated('Will be removed in 3.0. Use Ref instead')
+// ignore: unused_element
+mixin ConditionCreateSubjectiveRef on AutoDisposeFutureProviderRef<void> {
+  /// The parameter `record` of this provider.
+  String get record;
+}
+
+class _ConditionCreateSubjectiveProviderElement
+    extends AutoDisposeFutureProviderElement<void>
+    with ConditionCreateSubjectiveRef {
+  _ConditionCreateSubjectiveProviderElement(super.provider);
+
+  @override
+  String get record => (origin as ConditionCreateSubjectiveProvider).record;
+}
+// ignore_for_file: type=lint
+// ignore_for_file: subtype_of_sealed_class, invalid_use_of_internal_member, invalid_use_of_visible_for_testing_member, deprecated_member_use_from_same_package
--- app/lib/features/condition/presentation/condition_page.dart
+++ app/lib/features/condition/presentation/condition_page.dart
@@ -1,5 +1,9 @@
+import 'package:adaptive_dialog/adaptive_dialog.dart';
 import 'package:blooms/features/authentication/application/auth_providers.dart';
+import 'package:blooms/features/condition/application/condition_providers.dart';
+import 'package:blooms/features/condition/domain/condition.dart';
 import 'package:blooms/features/user/application/user_providers.dart';
+import 'package:firebase_ui_firestore/firebase_ui_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 
@@ -10,8 +14,8 @@ class ConditionPage extends HookConsumerWidget {
   Widget build(BuildContext context, WidgetRef ref) {
     return Scaffold(
       appBar: AppBar(),
-      body: Center(
-        child: Column(
+      drawer: Drawer(
+        child: ListView(
           children: [
             ElevatedButton(
               onPressed: () async {
@@ -27,6 +31,42 @@ class ConditionPage extends HookConsumerWidget {
             ),
           ],
         ),
+      ),
+      body: ref.watch(conditionQueryProvider).maybeWhen(
+            orElse: () => const CircularProgressIndicator.adaptive(),
+            data: (query) => FirestoreListView<Condition>(
+              query: query,
+              itemBuilder: (context, snapshot) {
+                final condition = snapshot.data();
+
+                return ListTile(
+                  title: Text(condition.record ?? ''),
+                  subtitle: Text(
+                    condition.createdAt?.toDate().toIso8601String() ?? '',
+                  ),
+                );
+              },
+            ),
+          ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () async {
+          final result = await showTextInputDialog(
+            context: context,
+            textFields: [
+              const DialogTextField(),
+            ],
+            title: '今の体調はどうですか？',
+            okLabel: '記録',
+          );
+
+          final record = result?.firstOrNull?.trim();
+          if (record != null && record.isNotEmpty) {
+            await ref.read(
+              conditionCreateSubjectiveProvider(record: record).future,
+            );
+          }
+        },
+        child: const Icon(Icons.add),
       ),
     );
   }
