3a96c1cac047095a198b06c3748f60b7a5fd5ef1
KoheiKanagu <kanagu@kingu.dev>
2025-01-21 10:42:04 +0900

    migration

---
+++ firebase/functions/src/admin/conditionsMigration.ts
@@ -0,0 +1,55 @@
+import * as admin from 'firebase-admin';
+import { Timestamp } from 'firebase-admin/firestore';
+import { logger } from 'firebase-functions';
+import { Condition, conditionConverter } from '../models/condition';
+import { CollectionPath } from '../utils/collectionPath';
+
+// process.env['GOOGLE_APPLICATION_CREDENTIALS'] = '../secrets/blooms-prod-43d74-firebase-adminsdk-jh3i4-6dfb8d09a7.json';
+process.env['GOOGLE_APPLICATION_CREDENTIALS'] = '../secrets/blooms-stg-firebase-adminsdk-4o7mx-3947c5abfa.json';
+admin.initializeApp();
+
+const raw: {
+  date: Date;
+  record: string;
+}[]
+  = [
+    { date: new Date('2025-01-21T10:00:00+09:00'), record: '頭痛がする' },
+  ];
+
+function create(
+  date: Date,
+  record: string,
+): Condition {
+  return new Condition(
+    Timestamp.fromDate(date),
+    Timestamp.fromDate(date),
+    null,
+    'uid',
+    'subjective',
+    [],
+    'success',
+    record,
+  );
+}
+
+/**
+ * Firestoreにデータをインポートする
+ */
+async function main(): Promise<void> {
+  const conditions: Condition[] = raw.map(e => create(e.date, e.record));
+
+  const collection = admin
+    .firestore()
+    .collection(CollectionPath.CONDITIONS)
+    .withConverter(conditionConverter);
+
+  for (const condition of conditions) {
+    logger.debug(condition);
+    await collection.add(condition);
+  }
+}
+
+// eslint-disable-next-line @typescript-eslint/no-floating-promises
+main().then(() => {
+  logger.info('Done');
+});
