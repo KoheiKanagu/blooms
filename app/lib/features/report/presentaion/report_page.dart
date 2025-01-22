017cda455f3d598e4819ef4660910f6f24b5b557
KoheiKanagu <kanagu@kingu.dev>
2025-01-22 14:27:02 +0900

    refactor

--- app/lib/features/report/presentaion/report_page.dart
+++ app/lib/features/report/presentaion/report_page.dart
@@ -1,12 +1,10 @@
 import 'package:blooms/features/report/application/report_providers.dart';
 import 'package:blooms/features/report/domain/report.dart';
-import 'package:blooms/features/report/domain/report_type.dart';
-import 'package:blooms/features/report/presentaion/report_type_badge.dart';
+import 'package:blooms/features/report/presentaion/report_page_list_tile.dart';
 import 'package:blooms/gen/strings.g.dart';
 import 'package:firebase_ui_firestore/firebase_ui_firestore.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:gap/gap.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 
 class ReportPage extends HookConsumerWidget {
@@ -32,65 +30,10 @@ class ReportPage extends HookConsumerWidget {
                 final report = snapshot.data();
                 final reportPeriod = report.reportPeriod;
 
-                return CupertinoListSection.insetGrouped(
-                  children: [
-                    CupertinoListTile.notched(
-                      backgroundColor:
-                          CupertinoColors.systemBackground.resolveFrom(context),
-                      title: Padding(
-                        padding: const EdgeInsets.symmetric(
-                          vertical: 8,
-                        ),
-                        child: Column(
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: [
-                            const Text(
-                              // TODO: summary
-                              '午前中に眠気、喉の痛み、食欲の増加が確認されました。',
-                              maxLines: 10,
-                            ),
-                            const Gap(8),
-                            ReportTypeBadge(report.type),
-                          ],
-                        ),
-                      ),
-                      trailing: Row(
-                        children: [
-                          Column(
-                            children: [
-                              Text(
-                                reportPeriod.startDate,
-                                style: CupertinoTheme.of(context)
-                                    .textTheme
-                                    .textStyle
-                                    .copyWith(
-                                      fontSize: 14,
-                                      color: CupertinoColors.label
-                                          .resolveFrom(context),
-                                    ),
-                              ),
-                              Visibility(
-                                visible: report.type != ReportType.past1day,
-                                child: Text(
-                                  reportPeriod.endDate,
-                                  style: CupertinoTheme.of(context)
-                                      .textTheme
-                                      .textStyle
-                                      .copyWith(
-                                        fontSize: 14,
-                                        color: CupertinoColors.secondaryLabel
-                                            .resolveFrom(context),
-                                      ),
-                                ),
-                              ),
-                            ],
-                          ),
-                          const Icon(CupertinoIcons.forward),
-                        ],
-                      ),
-                      onTap: () {},
-                    ),
-                  ],
+                return ReportPageListTile(
+                  report: report,
+                  reportPeriod: reportPeriod,
+                  onPressed: () {},
                 );
               },
             ),
---
+++ app/lib/features/report/presentaion/report_page_list_tile.dart
@@ -0,0 +1,84 @@
+import 'package:blooms/features/report/domain/report.dart';
+import 'package:blooms/features/report/domain/report_type.dart';
+import 'package:blooms/features/report/presentaion/report_type_badge.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:gap/gap.dart';
+
+class ReportPageListTile extends StatelessWidget {
+  const ReportPageListTile({
+    required this.report,
+    required this.reportPeriod,
+    required this.onPressed,
+    super.key,
+  });
+
+  final Report report;
+
+  final ({
+    String endDate,
+    String startDate,
+  }) reportPeriod;
+
+  final VoidCallback onPressed;
+
+  @override
+  Widget build(BuildContext context) {
+    return CupertinoListSection.insetGrouped(
+      children: [
+        CupertinoListTile.notched(
+          backgroundColor:
+              CupertinoColors.systemBackground.resolveFrom(context),
+          title: Padding(
+            padding: const EdgeInsets.symmetric(
+              vertical: 8,
+            ),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                const Text(
+                  // TODO: summary
+                  '午前中に眠気、喉の痛み、食欲の増加が確認されました。',
+                  maxLines: 10,
+                ),
+                const Gap(8),
+                ReportTypeBadge(report.type),
+              ],
+            ),
+          ),
+          trailing: Row(
+            children: [
+              Column(
+                children: [
+                  Text(
+                    reportPeriod.startDate,
+                    style:
+                        CupertinoTheme.of(context).textTheme.textStyle.copyWith(
+                              fontSize: 14,
+                              color: CupertinoColors.label.resolveFrom(context),
+                            ),
+                  ),
+                  Visibility(
+                    visible: report.type != ReportType.past1day,
+                    child: Text(
+                      reportPeriod.endDate,
+                      style: CupertinoTheme.of(context)
+                          .textTheme
+                          .textStyle
+                          .copyWith(
+                            fontSize: 14,
+                            color: CupertinoColors.secondaryLabel
+                                .resolveFrom(context),
+                          ),
+                    ),
+                  ),
+                ],
+              ),
+              const Icon(CupertinoIcons.forward),
+            ],
+          ),
+          onTap: onPressed,
+        ),
+      ],
+    );
+  }
+}
