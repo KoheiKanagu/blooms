4601ad821e2cb5cc81d6ad88e7067051283bd0e7
KoheiKanagu <kanagu@kingu.dev>
2025-01-22 16:17:02 +0900

    fix

--- app/lib/theme/my_decoration.dart
+++ app/lib/theme/my_decoration.dart
@@ -4,8 +4,9 @@ class MyDecoration {
   static BoxDecoration dropShadow(BuildContext context) => BoxDecoration(
         boxShadow: [
           BoxShadow(
-            color:
-                CupertinoColors.systemGrey.resolveFrom(context).withOpacity(1),
+            color: CupertinoColors.systemGrey
+                .resolveFrom(context)
+                .withOpacity(0.1),
             blurRadius: 8,
           ),
         ],
