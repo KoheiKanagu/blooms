db84194d201fc99e52576ae8ef19849f521ad196
KoheiKanagu <kanagu@kingu.dev>
2025-01-23 09:59:14 +0900

    feat: 体調のListをreverse

--- app/lib/features/condition/presentation/condition_page.dart
+++ app/lib/features/condition/presentation/condition_page.dart
@@ -3,6 +3,7 @@ import 'package:blooms/features/condition/application/condition_providers.dart';
 import 'package:blooms/features/condition/domain/condition.dart';
 import 'package:blooms/features/condition/presentation/condition_form.dart';
 import 'package:blooms/features/user/application/user_providers.dart';
+import 'package:blooms/theme/my_decoration.dart';
 import 'package:firebase_ui_firestore/firebase_ui_firestore.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
@@ -16,61 +17,68 @@ class ConditionPage extends HookConsumerWidget {
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    return Scaffold(
-      floatingActionButton: Container(
-        padding: EdgeInsets.only(
-          left: 8,
-          right: 8,
-          // キーボードが表示されていたら0
-          bottom: MediaQuery.of(context).viewInsets.bottom > 0 ? 0 : 50,
-        ),
-        child: const ConditionForm(),
-      ),
-      floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
-      body: CupertinoPageScaffold(
-        backgroundColor:
-            CupertinoColors.systemGroupedBackground.resolveFrom(context),
-        navigationBar: CupertinoNavigationBar(
-          middle: const Text('今日の体調'),
-          trailing: PullDownButton(
-            itemBuilder: (context) => [
-              PullDownMenuItem(
-                onTap: () {
-                  ref.read(authSignOutProvider.future);
-                },
-                title: 'Sign out',
-              ),
-              PullDownMenuItem(
-                onTap: () {
-                  ref.read(userDeleteProvider.future);
-                },
-                title: 'User delete',
-              ),
-            ],
-            buttonBuilder: (context, showMenu) => CupertinoButton(
-              onPressed: showMenu,
-              padding: EdgeInsets.zero,
-              child: const Icon(CupertinoIcons.ellipsis_circle),
+    return CupertinoPageScaffold(
+      backgroundColor:
+          CupertinoColors.systemGroupedBackground.resolveFrom(context),
+      navigationBar: CupertinoNavigationBar(
+        middle: const Text('今日の体調'),
+        trailing: PullDownButton(
+          itemBuilder: (context) => [
+            PullDownMenuItem(
+              onTap: () {
+                ref.read(authSignOutProvider.future);
+              },
+              title: 'Sign out',
             ),
+            PullDownMenuItem(
+              onTap: () {
+                ref.read(userDeleteProvider.future);
+              },
+              title: 'User delete',
+            ),
+          ],
+          buttonBuilder: (context, showMenu) => CupertinoButton(
+            onPressed: showMenu,
+            padding: EdgeInsets.zero,
+            child: const Icon(CupertinoIcons.ellipsis_circle),
           ),
         ),
-        child: ref.watch(conditionQueryProvider).maybeWhen(
-              orElse: () => const CircularProgressIndicator.adaptive(),
-              data: (query) => FirestoreListView<Condition>(
-                query: query,
-                itemBuilder: (context, snapshot) {
-                  final condition = snapshot.data();
+      ),
+      child: ref.watch(conditionQueryProvider).maybeWhen(
+            orElse: () => const CircularProgressIndicator.adaptive(),
+            data: (query) => SafeArea(
+              bottom: MediaQuery.of(context).viewInsets.bottom < 50,
+              child: Scaffold(
+                body: FirestoreListView<Condition>(
+                  query: query,
+                  reverse: true,
+                  padding: const EdgeInsets.only(
+                    // FloatingActionButton分のpadding
+                    bottom: 96,
+                  ),
+                  itemBuilder: (context, snapshot) {
+                    final condition = snapshot.data();
 
-                  return CupertinoListTile(
-                    title: Text(condition.record ?? ''),
-                    subtitle: Text(
-                      condition.createdAt?.toDate().toIso8601String() ?? '',
-                    ),
-                  );
-                },
+                    return CupertinoListTile(
+                      title: Text(condition.record ?? ''),
+                      subtitle: Text(
+                        condition.createdAt?.toDate().toIso8601String() ?? '',
+                      ),
+                    );
+                  },
+                ),
+                floatingActionButton: Container(
+                  padding: const EdgeInsets.symmetric(
+                    horizontal: 16,
+                  ),
+                  decoration: MyDecoration.dropShadow(context),
+                  child: const ConditionForm(),
+                ),
+                floatingActionButtonLocation:
+                    FloatingActionButtonLocation.centerFloat,
               ),
             ),
-      ),
+          ),
     );
   }
 }
