641321985307d23a160e0fb174f6884dac78335f
KoheiKanagu <kanagu@kingu.dev>
2025-01-25 13:15:55 +0900

    feat: dark theme

--- app/lib/theme/my_theme.dart
+++ app/lib/theme/my_theme.dart
@@ -5,6 +5,11 @@ const myLightThemeData = CupertinoThemeData(
   primaryColor: Color(0xFF3C8789),
 );
 
+const myDarkThemeData = CupertinoThemeData(
+  brightness: Brightness.dark,
+  primaryColor: Color(0xFF9ed094),
+);
+
 const myColorPink1 = Color(0xFFfac7b4);
 const myColorPink2 = Color(0xFFf8a993);
 const myColorPink3 = Color(0xFFd8816c);
