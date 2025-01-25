80dd827bc006000b68af5df241702eb286100517
KoheiKanagu <kanagu@kingu.dev>
2025-01-25 16:28:50 +0900

    rm

--- app/lib/features/highlight/domain/highlight.dart
+++ app/lib/features/highlight/domain/highlight.dart
@@ -6,7 +6,6 @@ import 'package:blooms/utils/timestamp_converter.dart';
 import 'package:blooms/utils/typedefs.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:freezed_annotation/freezed_annotation.dart';
-import 'package:intl/intl.dart';
 
 part 'highlight.freezed.dart';
 part 'highlight.g.dart';
