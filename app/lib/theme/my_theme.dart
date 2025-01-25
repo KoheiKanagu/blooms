a485b5a3b5f4da9630535c0ccc0fb9a9ac49e3fd
KoheiKanagu <kanagu@kingu.dev>
2025-01-25 14:12:57 +0900

    fix

--- app/lib/theme/my_theme.dart
+++ app/lib/theme/my_theme.dart
@@ -2,15 +2,35 @@ import 'package:flutter/cupertino.dart';
 
 const myLightThemeData = CupertinoThemeData(
   brightness: Brightness.light,
-  primaryColor: Color(0xFF3C8789),
+  primaryColor: myColorGreen2,
 );
 
 const myDarkThemeData = CupertinoThemeData(
   brightness: Brightness.dark,
-  primaryColor: Color(0xFF9ed094),
+  primaryColor: myColorGreen2,
 );
 
-const myColorPink1 = Color(0xFFfac7b4);
-const myColorPink2 = Color(0xFFf8a993);
-const myColorPink3 = Color(0xFFd8816c);
-const myColorGreen1 = Color(0xFF9ed094);
+const myColorPink1 = CupertinoDynamicColor.withBrightness(
+  color: Color.fromARGB(255, 250, 199, 180),
+  darkColor: Color.fromARGB(255, 250, 209, 190),
+);
+
+const myColorPink2 = CupertinoDynamicColor.withBrightness(
+  color: Color.fromARGB(255, 248, 169, 147),
+  darkColor: Color.fromARGB(255, 248, 179, 157),
+);
+
+const myColorPink3 = CupertinoDynamicColor.withBrightness(
+  color: Color.fromARGB(255, 216, 129, 108),
+  darkColor: Color.fromARGB(255, 216, 139, 118),
+);
+
+const myColorGreen1 = CupertinoDynamicColor.withBrightness(
+  color: Color.fromARGB(255, 158, 208, 148),
+  darkColor: Color.fromARGB(255, 154, 218, 147),
+);
+
+const myColorGreen2 = CupertinoDynamicColor.withBrightness(
+  color: Color.fromARGB(255, 60, 135, 137),
+  darkColor: Color.fromARGB(255, 56, 145, 138),
+);
