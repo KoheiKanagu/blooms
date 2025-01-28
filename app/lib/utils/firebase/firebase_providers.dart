9cbdc628672734ac040af3fd5c140c075e77ea05
KoheiKanagu <kanagu@kingu.dev>
2025-01-28 17:32:13 +0900

    storage

--- app/lib/utils/firebase/firebase_providers.dart
+++ app/lib/utils/firebase/firebase_providers.dart
@@ -2,6 +2,7 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_crashlytics/firebase_crashlytics.dart';
 import 'package:firebase_messaging/firebase_messaging.dart';
+import 'package:firebase_storage/firebase_storage.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:riverpod_annotation/riverpod_annotation.dart';
 
@@ -19,3 +20,6 @@ FirebaseAuth firebaseAuth(Ref ref) => FirebaseAuth.instance;
 
 @riverpod
 FirebaseMessaging firebaseMessaging(Ref ref) => FirebaseMessaging.instance;
+
+@riverpod
+FirebaseStorage firebaseStorage(Ref ref) => FirebaseStorage.instance;
--- app/lib/utils/firebase/firebase_providers.g.dart
+++ app/lib/utils/firebase/firebase_providers.g.dart
@@ -79,5 +79,22 @@ final firebaseMessagingProvider =
 @Deprecated('Will be removed in 3.0. Use Ref instead')
 // ignore: unused_element
 typedef FirebaseMessagingRef = AutoDisposeProviderRef<FirebaseMessaging>;
+String _$firebaseStorageHash() => r'47903c48019f7dfa1ba82fa0a905885442d69f6b';
+
+/// See also [firebaseStorage].
+@ProviderFor(firebaseStorage)
+final firebaseStorageProvider = AutoDisposeProvider<FirebaseStorage>.internal(
+  firebaseStorage,
+  name: r'firebaseStorageProvider',
+  debugGetCreateSourceHash: const bool.fromEnvironment('dart.vm.product')
+      ? null
+      : _$firebaseStorageHash,
+  dependencies: null,
+  allTransitiveDependencies: null,
+);
+
+@Deprecated('Will be removed in 3.0. Use Ref instead')
+// ignore: unused_element
+typedef FirebaseStorageRef = AutoDisposeProviderRef<FirebaseStorage>;
 // ignore_for_file: type=lint
 // ignore_for_file: subtype_of_sealed_class, invalid_use_of_internal_member, invalid_use_of_visible_for_testing_member, deprecated_member_use_from_same_package
