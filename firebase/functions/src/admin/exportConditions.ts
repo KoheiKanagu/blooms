091dc033b9f69a7a786c2dd58080890ce42d4c09
KoheiKanagu <kanagu@kingu.dev>
2025-02-01 14:11:41 +0900

    feat: 分析の対象期間を設定

--- firebase/functions/src/admin/exportConditions.ts
+++ firebase/functions/src/admin/exportConditions.ts
@@ -1,7 +1,6 @@
 import * as admin from 'firebase-admin';
-import { Timestamp } from 'firebase-admin/firestore';
 import { logger } from 'firebase-functions';
-import { ConditionContentText, conditionConverter } from '../condition/domain/condition';
+import { conditionConverter } from '../condition/domain/condition';
 import { CollectionPath } from '../utils/collectionPath';
 import { adminInitializeAppStg } from './adminInitializeApp';
 
@@ -19,21 +18,22 @@ async function main(): Promise<void> {
     .collection(CollectionPath.CONDITIONS)
     .withConverter(conditionConverter)
     .where('deletedAt', '==', null)
-    .where('content.type', '==', 'text')
     .where('subjectUid', '==', sourceUid)
-    .orderBy('createdAt', 'desc')
+    .orderBy('createdAt', 'asc')
     .get();
 
-  // JSONとして保存
   const records = results
     .docs
     .map(e => e.data())
-    .map(e => ({
-      日時: (e.createdAt as Timestamp).toDate().toISOString(),
-      記録: (e.content as ConditionContentText).text,
-    }));
+    .map((e) => {
+      if (e.content.type === 'text') {
+        return `${e.createdAtIso8601!}に入力したテキスト: ${e.content.text}`;
+      }
+      return null;
+    })
+    .filter(record => record !== null);
 
-  console.log(JSON.stringify(records, null, 2));
+  console.log(records);
 }
 
 // eslint-disable-next-line @typescript-eslint/no-floating-promises
