38f50dbac06dc79475fc148c81ef5b15a89cfd92
KoheiKanagu <kanagu@kingu.dev>
2025-02-03 13:33:33 +0900

    fix

--- app/lib/theme/my_theme.dart
+++ app/lib/theme/my_theme.dart
@@ -12,25 +12,25 @@ final myDarkThemeData = CupertinoThemeData(
 
 const myColorPink1 = CupertinoDynamicColor.withBrightness(
   color: Color.fromARGB(255, 250, 199, 180),
-  darkColor: Color.fromARGB(255, 250, 209, 190),
+  darkColor: Color.fromARGB(255, 248, 169, 147),
 );
 
 const myColorPink2 = CupertinoDynamicColor.withBrightness(
   color: Color.fromARGB(255, 248, 169, 147),
-  darkColor: Color.fromARGB(255, 248, 179, 157),
+  darkColor: Color.fromARGB(255, 216, 129, 108),
 );
 
 const myColorPink3 = CupertinoDynamicColor.withBrightness(
   color: Color.fromARGB(255, 216, 129, 108),
-  darkColor: Color.fromARGB(255, 216, 139, 118),
+  darkColor: Color.fromARGB(255, 250, 199, 180),
 );
 
 const myColorGreen1 = CupertinoDynamicColor.withBrightness(
   color: Color.fromARGB(255, 158, 208, 148),
-  darkColor: Color.fromARGB(255, 154, 218, 147),
+  darkColor: Color.fromARGB(255, 60, 135, 137),
 );
 
 const myColorGreen2 = CupertinoDynamicColor.withBrightness(
   color: Color.fromARGB(255, 60, 135, 137),
-  darkColor: Color.fromARGB(255, 56, 145, 138),
+  darkColor: Color.fromARGB(255, 158, 208, 148),
 );
