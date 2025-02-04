bdd08b3b3b8b1713f7cba923f3e80c8d2b28b04b
KoheiKanagu <kanagu@kingu.dev>
2025-02-04 13:32:26 +0900

    fix

--- app/lib/utils/firebase/firebase_providers.g.dart
+++ app/lib/utils/firebase/firebase_providers.g.dart
@@ -235,5 +235,23 @@ class _FirebaseStorageGsFileDownloadUrlProviderElement
   String get fileUri =>
       (origin as FirebaseStorageGsFileDownloadUrlProvider).fileUri;
 }
+
+String _$firebaseFunctionsHash() => r'd93861c0944a5de507eaed64d4c4ecf0b90ae818';
+
+/// See also [firebaseFunctions].
+@ProviderFor(firebaseFunctions)
+final firebaseFunctionsProvider = Provider<FirebaseFunctions>.internal(
+  firebaseFunctions,
+  name: r'firebaseFunctionsProvider',
+  debugGetCreateSourceHash: const bool.fromEnvironment('dart.vm.product')
+      ? null
+      : _$firebaseFunctionsHash,
+  dependencies: null,
+  allTransitiveDependencies: null,
+);
+
+@Deprecated('Will be removed in 3.0. Use Ref instead')
+// ignore: unused_element
+typedef FirebaseFunctionsRef = ProviderRef<FirebaseFunctions>;
 // ignore_for_file: type=lint
 // ignore_for_file: subtype_of_sealed_class, invalid_use_of_internal_member, invalid_use_of_visible_for_testing_member, deprecated_member_use_from_same_package
--- firebase/functions/src/utils/storagePath.ts
+++ firebase/functions/src/utils/storagePath.ts
@@ -1,3 +1,5 @@
 export const StoragePath = {
   PROMPTS: 'prompts_v1',
+  IMAGES: 'images_v1',
+  AUDIOS: 'audios_v1',
 };
