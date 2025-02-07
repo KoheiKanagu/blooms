eb41f14049f1f7b2078378896f9a984b39e53110
KoheiKanagu <kanagu@kingu.dev>
2025-02-07 14:34:58 +0900

    utils

---
+++ firebase/functions/test/utils/utils.ts
@@ -0,0 +1,25 @@
+import { assertFails, assertSucceeds } from '@firebase/rules-unit-testing';
+
+export function getFirestoreCoverageMeta(
+  projectId: string,
+) {
+  return `http://localhost:8080/emulator/v1/projects/${projectId}:ruleCoverage.html`;
+}
+
+export async function expectFirestorePermissionDenied(
+  promise: Promise<unknown>,
+) {
+  // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
+  const errorResult = await assertFails(promise);
+  // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access
+  expect(errorResult.code as unknown).toBe(
+    'PERMISSION_DENIED',
+  );
+}
+
+export async function expectFirestorePermissionSucceeds(
+  promise: Promise<unknown>,
+) {
+  const successResult = await assertSucceeds(promise);
+  expect(successResult).toBeDefined();
+}
