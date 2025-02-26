8f9089418a7809222b92b69765d8b3ac31609174
KoheiKanagu <kanagu@kingu.dev>
2025-02-26 10:07:49 +0900

    move

--- firebase/functions/test/jest.setup.ts
+++ firebase/functions/test/jest.setup.ts
@@ -1,3 +1,9 @@
+import { initializeApp } from 'firebase-admin/app';
+
 process.env['FIREBASE_AUTH_EMULATOR_HOST'] = '127.0.0.1:9099';
 process.env['FIRESTORE_EMULATOR_HOST'] = '127.0.0.1:8080';
 process.env['FIREBASE_STORAGE_EMULATOR_HOST'] = '127.0.0.1:9199';
+
+initializeApp({
+  storageBucket: 'test-bucket',
+});
