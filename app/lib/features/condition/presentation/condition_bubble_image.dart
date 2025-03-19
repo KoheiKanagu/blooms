6e24bad8c80ef758fcd932abdddd3d67dab96ce7
KoheiKanagu <kanagu@kingu.dev>
2025-03-20 00:15:28 +0900

    refactor: ConditionBubbleImage (#266)

--- app/lib/features/condition/presentation/condition_bubble_image.dart
+++ app/lib/features/condition/presentation/condition_bubble_image.dart
@@ -1,7 +1,5 @@
 import 'package:adaptive_dialog/adaptive_dialog.dart';
-import 'package:blooms/features/condition/domain/condition_content.dart';
-import 'package:blooms/features/condition/domain/condition_creator_type.dart';
-import 'package:blooms/features/condition/presentation/condition_bubble_blooms_icon.dart';
+import 'package:blooms/features/condition/domain/condition_content_image_attachment.dart';
 import 'package:blooms/gen/strings.g.dart';
 import 'package:blooms/utils/firebase/firebase_providers.dart';
 import 'package:cached_network_image/cached_network_image.dart';
@@ -10,88 +8,60 @@ import 'package:flutter_blurhash/flutter_blurhash.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 
 class ConditionBubbleImage extends HookConsumerWidget {
-  const ConditionBubbleImage(
-    this.content, {
-    required this.creatorType,
-    super.key,
-  });
+  const ConditionBubbleImage(this.attachment, {super.key});
 
-  final ConditionContentImage content;
-
-  final ConditionCreatorType creatorType;
+  final ConditionContentImageAttachment attachment;
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    return Column(
-      children: [
-        if (creatorType == ConditionCreatorType.model)
-          const ConditionBubbleBloomsIcon(),
-        Column(
-          spacing: 4,
-          children:
-              content.attachments
-                  .map<Widget>((attachment) {
-                    final placeholder = Image(
-                      image: BlurHashImage(
-                        attachment.additionalInfo.blurHash ?? '',
-                        decodingHeight: attachment.height,
-                        decodingWidth: attachment.width,
-                      ),
-                    );
-
-                    return ref
-                        .watch(
-                          firebaseStorageGsFileDownloadUrlProvider(
-                            fileUri: attachment.fileUri,
-                          ),
-                        )
-                        .maybeWhen(
-                          orElse: () => placeholder,
-                          data:
-                              (url) => CachedNetworkImage(
-                                imageUrl: url,
-                                placeholder: (context, url) => placeholder,
-                                errorWidget:
-                                    (context, url, error) => Stack(
-                                      alignment: Alignment.center,
-                                      children: [
-                                        placeholder,
-                                        CupertinoButton(
-                                          color: CupertinoColors.white,
-                                          sizeStyle: CupertinoButtonSize.small,
-                                          padding: EdgeInsets.zero,
-                                          onPressed: () {
-                                            showOkAlertDialog(
-                                              context: context,
-                                              title: i18n.imageLoadFailed,
-                                              style: AdaptiveStyle.iOS,
-                                              routeSettings:
-                                                  const RouteSettings(
-                                                    name: 'imageLoadFailed',
-                                                  ),
-                                            );
-                                          },
-                                          child: Icon(
-                                            CupertinoIcons.clear_circled_solid,
-                                            color: CupertinoColors.systemRed
-                                                .resolveFrom(context),
-                                            size: 32,
-                                          ),
-                                        ),
-                                      ],
-                                    ),
-                              ),
-                        );
-                  })
-                  .map(
-                    (e) => ClipRRect(
-                      borderRadius: BorderRadius.circular(10),
-                      child: e,
-                    ),
-                  )
-                  .toList(),
-        ),
-      ],
+    final placeholder = Image(
+      image: BlurHashImage(
+        attachment.additionalInfo.blurHash ?? '',
+        decodingHeight: attachment.height,
+        decodingWidth: attachment.width,
+      ),
     );
+
+    return ref
+        .watch(
+          firebaseStorageGsFileDownloadUrlProvider(fileUri: attachment.fileUri),
+        )
+        .maybeWhen(
+          orElse: () => placeholder,
+          data:
+              (url) => CachedNetworkImage(
+                imageUrl: url,
+                placeholder: (context, url) => placeholder,
+                errorWidget:
+                    (context, url, error) => Stack(
+                      alignment: Alignment.center,
+                      children: [
+                        placeholder,
+                        CupertinoButton(
+                          color: CupertinoColors.white,
+                          sizeStyle: CupertinoButtonSize.small,
+                          padding: EdgeInsets.zero,
+                          onPressed: () {
+                            showOkAlertDialog(
+                              context: context,
+                              title: i18n.imageLoadFailed,
+                              style: AdaptiveStyle.iOS,
+                              routeSettings: const RouteSettings(
+                                name: 'imageLoadFailed',
+                              ),
+                            );
+                          },
+                          child: Icon(
+                            CupertinoIcons.clear_circled_solid,
+                            color: CupertinoColors.systemRed.resolveFrom(
+                              context,
+                            ),
+                            size: 32,
+                          ),
+                        ),
+                      ],
+                    ),
+              ),
+        );
   }
 }
