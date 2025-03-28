59d7410212c5229315c81dedcf033b79fbbc1b88
KoheiKanagu <kanagu@kingu.dev>
2025-03-28 15:24:47 +0900

    test: MaestroのテストでApp Checkを突破

--- app/.maestro/test.sh
+++ app/.maestro/test.sh
@@ -1,12 +1,25 @@
 #!/bin/bash
 set -euxo pipefail
 
+export SIMCTL_CHILD_FIRAAppCheckDebugToken="$APP_CHECK_DEBUG_TOKEN"
+
+TEST_DEVICE_NAME="E2E Test"
+
+# Build the app
 APP_PATH="build/ios/iphonesimulator/Runner.app"
 rm -rf "$APP_PATH"
-
 fvm flutter build ios --debug --simulator --flavor stg
 
-DEVICE_UDID=$(xcrun simctl list devices booted --json | jq -r '.devices[][0].udid')
+# Set Environment Variables
+DEVICE_UDID=$(xcrun simctl list devices --json | jq -r ".devices[][] | select(.name == \"$TEST_DEVICE_NAME\") | .udid")
+xcrun simctl shutdown "$DEVICE_UDID" || true
+xcrun simctl boot "$DEVICE_UDID"
+
+# Install the app
 xcrun simctl install "$DEVICE_UDID" "$APP_PATH"
 
+# Run Maestro tests
 maestro test .maestro/test.yml
+
+# Shutdown the Test Device
+xcrun simctl shutdown "$DEVICE_UDID" || true
